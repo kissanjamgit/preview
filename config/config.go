5382a84b77845f02ab242a737b1e6e7950484881
jack8096 <jackdeepakkamble@gmail.com>
2026-05-04 16:38:38 +0530

    +config

---
+++ config/config.go
@@ -0,0 +1,39 @@
+// Package config to load toml
+package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	Player     string `toml:"player"`
+	PlayerArgs string `toml:"playerArgs"`
+}
+
+func New() (c Config, err error) {
+	c = Config{Player: "vlc.exe", PlayerArgs: ""}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return
+	}
+	configPath := filepath.Join(homeDir, ".ext.toml")
+	b, err := os.ReadFile(configPath)
+	if os.IsNotExist(err) {
+		return c, nil
+	}
+
+	if err != nil {
+		return
+	}
+	var t Config
+
+	if err = toml.Unmarshal(b, &t); err != nil {
+		return
+	} else if (Config{}) != t { // if config is empty
+		c = t
+	}
+	return
+}

aa9b9c875108e8a1ede3edb6e0100e8490115cde
jack8096 <jackdeepakkamble@gmail.com>
2026-05-08 17:38:27 +0530

    xp

---
+++ xp/xp.go
@@ -0,0 +1,39 @@
+package xp
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/kissanjamgit/preview"
+	"resty.dev/v3"
+)
+
+type xp struct {
+	source string
+}
+
+func New(source string) preview.Preview {
+	return &xp{source}
+}
+
+var baseURL = `https://pornxp.ph`
+
+func (x *xp) Get(index int) (list []preview.ContentResource, err error) {
+	index += 1
+	uri := fmt.Sprintf(`%s/?page=%d`, baseURL, index)
+	client := resty.New()
+	defer client.Close()
+	res, err := client.R().Get(uri)
+	if err != nil {
+		return
+	}
+	submatch := regexp.MustCompile(`data-preview="//([^/]+/(\d+)\.mp4)"`).FindAllStringSubmatch(res.String(), -1)
+	for _, m := range submatch {
+		if len(m) < 3 {
+			continue
+		}
+		list = append(list, preview.ContentResource{Source: fmt.Sprintf(`%s/videos/%s`, baseURL, m[2]), View: fmt.Sprintf(`https://%s`, m[1])})
+	}
+
+	return
+}

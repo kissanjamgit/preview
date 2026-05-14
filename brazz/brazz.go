7fda7c64e7c41f44789f3bad62272c39f72c39f0
jack8096 <jackdeepakkamble@gmail.com>
2026-05-14 17:07:00 +0530

    +brazz

--- brazz/brazz.go
+++ brazz/brazz.go
@@ -14,7 +14,7 @@ import (
 	"resty.dev/v3"
 )
 
-var Domain = []string{`brazzers`, `milfed`, `bangbros`, `realitykings`, `twistys`, `digitalplayground`, `mofos`, `letsdoeit`, `mypervyfamily`}
+var Domain = []string{`brazzers`, `milfed`, `bangbros`, `realitykings`, `twistys`, `digitalplayground`, `mofos`, `letsdoeit`, `mypervyfamily`, `deviante`}
 
 type Brazz struct {
 	Source string

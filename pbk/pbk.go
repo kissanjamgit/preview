d9df170d8062b2724f58556fcd3c9bb69c49f212
jack8096 <jackdeepakkamble@gmail.com>
2026-06-06 18:15:33 +0530

    +pbk

---
+++ pbk/pbk.go
@@ -0,0 +1,47 @@
+// Package pbk something
+package pbk
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/kissanjamgit/preview"
+	"github.com/kissanjamgit/preview/config"
+	"resty.dev/v3"
+)
+
+type pbk struct {
+	Source string
+}
+
+func New() *pbk {
+	return &pbk{}
+}
+
+func (p *pbk) SetSource(source string) {
+	p.Source = source
+}
+
+func (*pbk) Name() string {
+	return "premium-bukkake"
+}
+
+func (p *pbk) Get(index int) (list []preview.ContentResource, err error) {
+	cfg := config.ConfigLazy
+	if cfg == nil {
+		err = fmt.Errorf("config.ConfigLazy == nil")
+		return
+	}
+	client := resty.New()
+
+	uri := "https://premiumbukkake.com/tour2/updates/page_1.html"
+	res, err := client.SetProxy(config.ConfigLazy.Proxy).R().Get(uri)
+	if err != nil {
+		return
+	}
+	submatch := regexp.MustCompile(`alt="([^"]*) Player Thumbnail">\s*<span[^>]*'([^']+mp4)'`).FindAllStringSubmatch(res.String(), -1)
+	for _, match := range submatch {
+		list = append(list, preview.ContentResource{Source: match[1], View: match[2]})
+	}
+	return
+}

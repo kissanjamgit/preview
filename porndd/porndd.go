549f56dcd3450f781537b6c7a108a8e2e46f2955
jack8096 <jackdeepakkamble@gmail.com>
2026-07-15 13:02:17 +0530

    chore: add regex placeholder for extraction logic

--- porndd/porndd.go
+++ porndd/porndd.go
@@ -22,5 +22,6 @@ func (p *porndd) Name() string {
 
 func (p *porndd) Get(index int) ([]preview.ContentResource, error) {
 	// Placeholder for extraction logic
+	// regex string <a href="([^"]*)">\s*<img [^>]*src="([^"]*)"
 	return []preview.ContentResource{}, nil
 }

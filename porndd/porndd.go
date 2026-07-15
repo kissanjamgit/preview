10ef71da8b77a0d0bc363eb248bc464875067d53
jack8096 <jackdeepakkamble@gmail.com>
2026-07-15 13:14:30 +0530

    feat: update regex and add debug file output for porndd scraper

--- porndd/porndd.go
+++ porndd/porndd.go
@@ -1,6 +1,7 @@
 package porndd
 
 import (
+	"os"
 	"regexp"
 
 	"github.com/kissanjamgit/preview"
@@ -26,28 +27,27 @@ func (p *porndd) Name() string {
 func (p *porndd) Get(index int) ([]preview.ContentResource, error) {
 	client := resty.New()
 	client.SetHeaders(map[string]string{
-		"User-Agent":                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0",
-		"Accept":                    "*/*",
-		"Accept-Language":           "en-US,en;q=0.9",
-		"Accept-Encoding":           "gzip, deflate, br, zstd",
-		"Referer":                   "https://porndd.com/",
-		"Sec-GPC":                   "1",
-		"Connection":                "keep-alive",
-		"Sec-Fetch-Dest":            "empty",
-		"Sec-Fetch-Mode":            "no-cors",
-		"Sec-Fetch-Site":            "same-origin",
-		"Priority":                  "u=4",
-		"Pragma":                    "no-cache",
-		"Cache-Control":             "no-cache",
-		"TE":                        "trailers",
+		"User-Agent":      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0",
+		"Accept":          "*/*",
+		"Accept-Language": "en-US,en;q=0.9",
+		"Referer":         "https://porndd.com/",
+		"Sec-GPC":         "1",
+		"Connection":      "keep-alive",
+		"Sec-Fetch-Dest":  "empty",
+		"Sec-Fetch-Mode":  "no-cors",
+		"Sec-Fetch-Site":  "same-origin",
+		"Priority":        "u=4",
+		"Pragma":          "no-cache",
+		"Cache-Control":   "no-cache",
+		"TE":              "trailers",
 	})
 
 	res, err := client.R().Get("https://porndd.com/?mode=async&function=get_block&block_id=list_videos_most_recent_videos&sort_by=post_date&from=1/")
 	if err != nil {
 		return nil, err
 	}
-
-	re := regexp.MustCompile(`<a href="([^"]*)">\s*<img [^>]*src="([^"]*)"`)
+	os.WriteFile(`context.html`, res.Bytes(), 0o644)
+	re := regexp.MustCompile(`<a href="([^"]*)"[^>]*>\s*<img [^>]*src="([^"]*)"`)
 	matches := re.FindAllStringSubmatch(res.String(), -1)
 
 	list := make([]preview.ContentResource, 0, len(matches))

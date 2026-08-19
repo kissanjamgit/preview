1ba1b360c5cb2e323916e0d45065f30b19be124d
jack8096 <jackdeepakkamble@gmail.com>
2026-08-19 18:40:21 +0530

    save progress

--- sxyprn/sxyprn.go
+++ sxyprn/sxyprn.go
@@ -4,6 +4,7 @@ package sxyprn
 import (
 	"fmt"
 	"net/url"
+	"slices"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -76,27 +77,69 @@ func (s *Sxyprn) parse(uri string) (list []preview.ContentResource, err error) {
 
 // --- Provider 2: Blog Site ---
 
-type SxyprnBlog struct{}
+var blog = map[string]string{`sxyprnBlog`: `all`, `ILUVY`: `5f3950a938042`, `AJ47`: `@AJ47`, `mikess`: `@mikess`, `Ajx`: `@Ajx`, `PornoBB`: `66326b4fdfd13`, `DemonINC`: `64d19fdf6970d`, `iAmFckr`: `667bfa40a223f`, `RIPNSFW`: `604868c9e12f6`, `sandy998`: `67b6a8c5c1227`, `scfc`: `6803728ea8837`, `VOOP`: `6a1f341252008`}
 
-func NewBlog() *SxyprnBlog                    { return &SxyprnBlog{} }
-func (s *SxyprnBlog) Name() string            { return "sxyprn-blog" }
-func (s *SxyprnBlog) SetSource(source string) {}
+type SxyprnBlog struct {
+	Source string
+}
+
+func NewBlog() *SxyprnBlog         { return &SxyprnBlog{} }
+func (s *SxyprnBlog) Name() string { return "sxyprn-blog" }
+func (s *SxyprnBlog) Domain() []string {
+	keys := make([]string, len(blog))
+	i := 0
+	for k := range blog {
+		keys[i] = k
+		i++
+	}
+	slices.Sort(keys)
+	return keys
+}
+func (s *SxyprnBlog) SetSource(source string) { s.Source = source }
 func (s *SxyprnBlog) Get(index int) (list []preview.ContentResource, err error) {
-	uri := fmt.Sprintf("https://sxyprn.net/blog/all/%d", 20*index)
+	uri := fmt.Sprintf("https://sxyprn.net/blog/%s/%d", blog[s.Source], 20*index)
 	d, err := fetch(uri)
 	if err != nil {
 		return
 	}
 
 	d.Find(".post_el_small").Each(func(i int, g *goquery.Selection) {
+		// 1. Corrected selector with dots
+		extlinkIcon := g.Find(`.extlink_icon.extlink`)
+
+		var hrefs []string
+		extlinkIcon.Each(func(i int, s *goquery.Selection) {
+			text, ok := s.Attr(`href`)
+			if ok {
+				hrefs = append(hrefs, text)
+			}
+		})
+
 		title := strings.TrimSpace(g.Find(".post_text").Text())
-		src, ok := g.Find(".hvp_player").Attr("src")
-		if ok {
-			list = append(list, preview.ContentResource{
-				Source: title,
-				View:   "https:" + src,
-			})
+		title = strings.ReplaceAll(title, "\n", " ")
+
+		// 2. Only add the links if we actually found any
+		if len(hrefs) > 0 {
+			title += " " + strings.Join(hrefs, " ")
 		}
+		var src string
+		if g.Find(".duration_small").Text() == `EXTERNAL LINK` {
+			if s, ok := g.Find(".mini_post_vid_thumb").Attr("data-src"); !ok {
+				return
+			} else {
+				src = s
+			}
+		} else {
+			if s, ok := g.Find(".hvp_player").Attr("src"); !ok {
+				return
+			} else {
+				src = s
+			}
+		}
+		list = append(list, preview.ContentResource{
+			Source: title,
+			View:   "https:" + src,
+		})
 	})
 	return
 }
@@ -105,18 +148,34 @@ func (s *SxyprnBlog) Get(index int) (list []preview.ContentResource, err error) 
 
 type SxyprnPopular struct{}
 
-func NewPopular() *SxyprnPopular                  { return &SxyprnPopular{} }
-func (s *SxyprnPopular) Name() string             { return "sxyprn-popular" }
-func (s *SxyprnPopular) SetSource(source string)  {}
+func NewPopular() *SxyprnPopular                 { return &SxyprnPopular{} }
+func (s *SxyprnPopular) Name() string            { return "sxyprn-popular" }
+func (s *SxyprnPopular) SetSource(source string) {}
 func (s *SxyprnPopular) Get(index int) (list []preview.ContentResource, err error) {
-	uri := fmt.Sprintf("https://sxyprn.net/popular/top-viewed/%d", 30*index)
+	uri := fmt.Sprintf("https://sxyprn.net/popular/top-viewed/%d?p=day", 30*index)
 	d, err := fetch(uri)
 	if err != nil {
 		return
 	}
 
 	d.Find(".post_el_small").Each(func(i int, g *goquery.Selection) {
+		extlinkIcon := g.Find(`.extlink_icon.extlink`)
+
+		var hrefs []string
+		extlinkIcon.Each(func(i int, s *goquery.Selection) {
+			text, ok := s.Attr(`href`)
+			if ok {
+				hrefs = append(hrefs, text)
+			}
+		})
+
 		title := strings.TrimSpace(g.Find(".post_text").Text())
+		title = strings.ReplaceAll(title, "\n", " ")
+
+		if len(hrefs) > 0 {
+			title += " " + strings.Join(hrefs, " ")
+		}
+
 		src, ok := g.Find(".hvp_player").Attr("src")
 		if ok {
 			list = append(list, preview.ContentResource{
